584b6a2f2b22a7a540f6e95bf862d74c1589f3f6
Jeremie-Doricent <6325048@cegepmontpetit.ca>
2026-05-08 15:33:19 -0400

    Tp3 Parti 3

--- TP3_Quiz_V6_Materiel/TP3_Quiz_V6_Materiel/Models/Question.cs
+++ TP3_Quiz_V6_Materiel/TP3_Quiz_V6_Materiel/Models/Question.cs
@@ -7,7 +7,65 @@ using Models.Interfaces;
 
 namespace Models
 {
-    internal class Question: IQuestion
+    public class Question: IQuestion
     {
+        private string m_enonce;
+        private int m_point;
+        private string m_option;
+
+        private string m_indice;
+        private int m_penaliteIndice;
+        public string Enonce 
+        {  get { return m_enonce; } 
+            set { if (value == null || value == "") 
+                { throw new ArgumentException(); }
+                    m_enonce = value.Trim();
+            }
+        }
+        public int Points { get { return m_point; } 
+            set { if (value < 0) 
+                { throw new ArgumentException(); } 
+            
+            m_point = value;
+            }
+        
+        }
+        public List<string> Options
+        {
+            get { return m_option; }
+            set
+            { if (value == null )  { throw new ArgumentNullException(); }
+            
+                if ( value.Count < 2)
+                {
+                    throw new ArgumentException();
+                }
+                m_option = value;
+            }
+        }
+         
+        public string Indice
+        {
+            get { return m_indice ; }
+            set
+            {
+                if (value == null) { throw new ArgumentException(); }
+
+            
+            m_indice = value;
+            }
+        }
+      
+        public int PenaliteIndice {
+
+            get { return m_penaliteIndice; }
+            set { if (value < 0 || value > 1) { throw new ArgumentOutOfRangeException(); }
+            
+            PenaliteIndice = value;
+            }
+        
+            
+        }
     }
+    
 }
