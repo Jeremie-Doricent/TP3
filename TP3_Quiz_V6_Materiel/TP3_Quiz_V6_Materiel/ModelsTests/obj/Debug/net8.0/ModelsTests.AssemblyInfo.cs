c9fb6edc32b500c4482845e6fe3bd0f4465d8af9
Jeremie-Doricent <6325048@cegepmontpetit.ca>
2026-05-23 14:24:46 -0400

    parti 3 tp3

--- TP3_Quiz_V6_Materiel/TP3_Quiz_V6_Materiel/ModelsTests/obj/Debug/net8.0/ModelsTests.AssemblyInfo.cs
+++ TP3_Quiz_V6_Materiel/TP3_Quiz_V6_Materiel/ModelsTests/obj/Debug/net8.0/ModelsTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModelsTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8a21a995f1e0c8cddb5c5e36b31379107d7b5e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aae2fb5879765c7fca5377608056223daa2a4c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModelsTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModelsTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
